2e10eb77652f311bb5791a3268bbace950adca64
Matt Chun-Lum <matt@questionable.engineering>
2023-07-06 18:16:44 -0700

    chore: add repo-sepecific build scripts

---
+++ ci/check-msrv.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -ex
+
+if command -v apk > /dev/null; then
+    apk add openssl \
+        openssl-dev
+else
+    apt-get install -y \
+        libssl-dev
+fi
+
+# We need to work around a current limitaiton in `cargo msrv` where it can't
+# handle verifying the version specified by a workspace.
+# https://github.com/foresterre/cargo-msrv/issues/590
+#
+# So what we're going to do is determine the minimum version that's declared in
+# the Cargo.toml at the root of the repository and explicitly constrain
+# `cargo msrv` to that version using `--min` and `--max`
+
+rust_version=$(grep 'rust-version' Cargo.toml | cut -d '"' -f 2)
+
+if [ "$UPDATE_DEPS" -eq 1 ]; then
+    cargo update
+fi
+
+# just a basic check
+cargo msrv --min=${rust_version} --max=${rust_version}
+
+# verify we can run tests at this version
+cargo msrv --min=${rust_version} --max=${rust_version} -- cargo test --locked --all-targets
---
+++ ci/lint-and-test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -ex
+
+if command -v apk > /dev/null; then
+    apk add openssl \
+        openssl-dev
+else
+    apt-get install -y \
+        libssl-dev
+fi
+
+if [ "$LINT" -eq 1 ]; then
+    # make sure we're formatted
+    cargo fmt --check
+
+    # fail on clippy warnings
+    cargo clippy -- -Dwarnings
+fi
+
+# ensure we can build
+cargo build --verbose ${EXTRA_CARGO_BUILD_FLAGS}
+
+# ensure tests pass
+cargo test --verbose ${EXTRA_CARGO_TEST_FLAGS}
